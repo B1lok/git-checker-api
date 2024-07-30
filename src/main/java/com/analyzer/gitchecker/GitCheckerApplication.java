b2cfa57dfccfc188d4bceedff49effd3a6ed9d4a
Bilokur Evgen <bilokur915@gmail.com>
2024-07-30 11:02:48 +0300

    Initial commit

---
+++ src/main/java/com/analyzer/gitchecker/GitCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.analyzer.gitchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCheckerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GitCheckerApplication.class, args);
+  }
+
+}
